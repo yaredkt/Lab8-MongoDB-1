108024c052c457298eec90afe13dd040c90a72cb
yaried tekie <yaredsonj@gmail.com>
2018-06-06 13:45:18 -0500

    indexes are created

--- ExpessAPI/exercise3/app.js
+++ ExpessAPI/exercise3/app.js
@@ -4,8 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongo = require('mongoskin');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var add = require('./routes/add');
+var update = require('./routes/update');
+var deleteLoc = require('./routes/delete');
+var search = require('./routes/search');
+// var searchResult = require('./routes/searchResult');
 
 var app = express();
 
@@ -13,18 +26,32 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use((req, res, next)=>{
+  var db = mongo.db('mongodb://localhost:27017/LocationDB', { native_parser: true });
+  req.dbs = db;
+  next();
+  db.close();
+});
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/users', users);
+app.use('/add', add);
+app.use('/update', update);
+app.use('/delete', deleteLoc);
+app.use('/search',search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
@@ -37,5 +64,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(9999);
 
 module.exports = app;
